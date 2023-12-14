0a5ef40c9e9deb284fd7bb496aaa597bc06a3a0a
1024umer <127780703+1024umer@users.noreply.github.com>
2023-12-15 00:49:44 +0500

    multiple pages added

--- lms_project/lms_project/urls.py
+++ lms_project/lms_project/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from django.urls import path
-from .import views
+from django.urls import path,include
+from .import views,auth
 urlpatterns = [
     path('admin/', admin.site.urls),
     
     path('base/', views.base, name='base'),
+    path('', views.home, name='home'),
+    path('course', views.course, name='course'),
+    path('contact-us', views.contact, name='contact'),
+    path('about-us', views.about, name='about'),
+    
+    path('auth/login',auth.login,name='login'),
+    path('auth/register',auth.register,name='register'),
 ]
--- lms_project/lms_project/views.py
+++ lms_project/lms_project/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import redirect,render
 
 def base(request):
     return render(request,'base.html')
+
+def home(request):
+    return render(request,'main/home.html')
+
+def course(request):
+    return render(request,'main/course.html')
+
+def contact(request):
+    return render(request,'main/contact-us.html')
+
+def about(request):
+    return render(request,'main/about-us.html')
